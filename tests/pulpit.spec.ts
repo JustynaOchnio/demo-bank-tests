d709432c64b625b474cccdf8b14608b223611515
JustynaOchnio <jochnio@sii.pl>
2025-06-12 12:43:39 +0200

    adding correct balance test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -9,7 +9,7 @@ test.describe('Pulpit tests', () => {
     await page.getByTestId('login-input').fill(userId);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
-  }); 
+  });
 
   test('quick payment with correct data', async ({ page }) => {
     //Arrange
@@ -35,6 +35,7 @@ test.describe('Pulpit tests', () => {
     //Arrange
     const transferAmount = '50';
     const receiverNumber = '500 xxx xxx';
+    const expectedMessage = `Doładowanie wykonane! ${transferAmount},00PLN na numer ${receiverNumber}`;
 
     //Act
     await page.locator('#widget_1_topup_receiver').selectOption(receiverNumber);
@@ -44,8 +45,25 @@ test.describe('Pulpit tests', () => {
     await page.getByTestId('close-button').click();
 
     //Assert
-    await expect(page.getByTestId('message-text')).toHaveText(
-      `Doładowanie wykonane! ${transferAmount},00PLN na numer ${receiverNumber}`,
-    );
+    await expect(page.getByTestId('message-text')).toHaveText(expectedMessage);
+  });
+
+  test('correct balance after successful mobile top-up', async ({ page }) => {
+    //Arrange
+    const transferAmount = '50';
+    const receiverNumber = '500 xxx xxx';
+    const expectedMessage = `Doładowanie wykonane! ${transferAmount},00PLN na numer ${receiverNumber}`;
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(transferAmount);
+
+    //Act
+    await page.locator('#widget_1_topup_receiver').selectOption(receiverNumber);
+    await page.locator('#widget_1_topup_amount').fill(transferAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').check();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    //Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
   });
 });
